551ed07bfdd384817d5db3dce9efdabb3706dac1
Ikram Aslam <ikramaslam1020@gmail.com>
2024-05-14 20:13:38 +0500

    Productdetail screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ProductScreen from "./src/screens/ProductScreen";
-
+import ProductDetail from "./src/screens/ProductDetail";
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductScreen />
+      {/*<ProductScreen /> */}
+      <ProductDetail />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
---
+++ src/screens/ProductDetail.js
@@ -0,0 +1,81 @@
+import {
+  View,
+  Image,
+  StyleSheet,
+  FlatList,
+  useWindowDimensions,
+  Text,
+  ScrollView,
+  Pressable,
+} from "react-native";
+import products from "../data/products";
+const ProductDetail = () => {
+  const product = products[0];
+  const { width } = useWindowDimensions();
+  const addToCard = () => [console.warn("ikram")];
+  return (
+    <View>
+      <ScrollView>
+        {/*Image Carousel */}
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image source={{ uri: item }} style={{ width, aspectRatio: 1 }} />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+        />
+        <View style={{ padding: 20 }}>
+          {/*Title*/}
+          <Text style={styles.title}>{product.name}</Text>
+
+          {/*Price*/}
+          <Text style={styles.price}>${product.price}</Text>
+
+          {/*Description*/}
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
+      <Pressable onPress={addToCard} style={styles.btn}>
+        <Text style={styles.btnTxt}>Add to Cart</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default ProductDetail;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 34,
+    fontWeight: "500s",
+    marginVertical: 10,
+  },
+  price: {
+    fontWeight: "500",
+    fontSize: 16,
+    letterSpacing: 1.5,
+  },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 25,
+    fontWeight: "300",
+  },
+  btn: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    padding: 20,
+    borderRadius: 100,
+    alignItems: "center",
+  },
+  btnTxt: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+});
